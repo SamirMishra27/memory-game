487301359ed8124262c095e27a025a12c08cfb44
Samir Mishra <samirmishra.a1@gmail.com>
2023-03-19 21:34:30 +0530

    chore: init tailwind config and remove vite code
    Add images.ts and utils.ts in src directory
    Init tailwind.config.cjs file to use tailwind
    No semi colon rule in eslint prettier.

---
+++ src/images.ts
@@ -0,0 +1,11 @@
+import crab from './assets/crab.png'
+import umbrella from './assets/umbrella.png'
+import pinetree from './assets/pinetree.jpg'
+import sunglass from './assets/sunglass.png'
+
+export const images = {
+    crab: crab,
+    umbrella: umbrella,
+    pinetree: pinetree,
+    sunglass: sunglass
+}
