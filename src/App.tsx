00d633254d4434edfad7cd77615c0376699f7ec5
Samir Mishra <samirmishra.a1@gmail.com>
2023-04-06 14:51:56 +0530

    fix: 2 meta tags not embedding
    * Move twitter:card and theme-color meta tags inside App.tsx's Helmet component so they don't disappear in hydration

--- src/App.tsx
+++ src/App.tsx
@@ -153,16 +153,18 @@ function App() {
                 />
 
                 <meta
-                    content="https://media.discordapp.net/attachments/795951827232358400/1092358566339428443/Screenshot_2023-03-23_012431.png"
+                    content={window.location.href + 'image.png'}
                     data-react-helmet="true"
                     property="og:image"
                 />
                 <meta content="image/png" data-react-helmet="true" property="og:image:type" />
                 <meta
-                    content="https://media.discordapp.net/attachments/795951827232358400/1092358566339428443/Screenshot_2023-03-23_012431.png"
+                    content={window.location.href + 'image.png'}
                     data-react-helmet="true"
                     property="twitter:image:src"
                 />
+                <meta content="summary_large_image" name="twitter:card" data-react-helmet="true" />
+                <meta content="#FFE483" name="theme-color" data-react-helmet="true" />
             </Helmet>
             <GameStats size={boardSize} moves={movesCount} stopwatch={stopwatch} />
             {boardSize && (
